cb9724073fbdd36cc5bbec267cc1f4bbb81a8473
Azees2023 <149593004+Azees2023@users.noreply.github.com>
2024-01-18 23:40:37 +0530

    Update CarCard.jsx

--- src/Components/CarCard.jsx
+++ src/Components/CarCard.jsx
@@ -29,11 +29,10 @@ const CarCard = () => {
                 <Link title='more' to='/Card'
                        className='ml-[30px]   font-bold  w-[150px] rounded bg-blue-500 px-4 py-3 text-white hover:bg-blue-700 flex float-left'>
                         Learn More
+                     <svg class="w-3.5 h-3.5 ms-3 m-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
                 </Link>
-              <svg class="w-3.5 h-3.5 ms-3 m-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-              </svg>
-              </a>
             </div>
           </div>
        </div>
