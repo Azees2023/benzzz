6ae2ab6c14a1e8513f0ebe34c6547d5a03f60e06
Azees2023 <149593004+Azees2023@users.noreply.github.com>
2024-01-18 23:33:29 +0530

    Update CarCard.jsx

--- src/Components/CarCard.jsx
+++ src/Components/CarCard.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { carDetail } from '../data';
+import { Link } from 'react-router-dom';
 
 
 const CarCard = () => {
@@ -24,7 +25,11 @@ const CarCard = () => {
                 {car.specification}
             </p>
             <div className='mb-16 items-center ml-12 md:mt-[100px]'>
-              <a href='/Card' target='_blank' className='ml-[30px]   font-bold  w-[150px] rounded bg-blue-500 px-4 py-3 text-white hover:bg-blue-700 flex float-left '>Learn More
+{/*               <a href='/Card' target='_blank' className='ml-[30px]   font-bold  w-[150px] rounded bg-blue-500 px-4 py-3 text-white hover:bg-blue-700 flex float-left '>Learn More */}
+                <Link title='more' to='/Card'
+                       className='ml-[30px]   font-bold  w-[150px] rounded bg-blue-500 px-4 py-3 text-white hover:bg-blue-700 flex float-left'>
+                        Learn More
+                </Link>
               <svg class="w-3.5 h-3.5 ms-3 m-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
               </svg>
